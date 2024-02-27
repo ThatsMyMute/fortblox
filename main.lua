18d1448805e504cd6e8d422189b9c38fe5f17649
ThatsMyMute <75107278+ThatsMyMute@users.noreply.github.com>
2024-02-27 13:16:27 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,18 @@
+--  should work :)
+local reg = 10154245153
+local br1 = 14907807948
+local br2 = 14907786755
+if game.PlaceId == reg then
+    local src = game:HttpGet("https://raw.githubusercontent.com/ThatsMyMute/fortblox/main/src.lua")
+    loadstring(src)()
+end
+
+if game.PlaceId == br1 then
+    local br = game:HttpGet("https://raw.githubusercontent.com/ThatsMyMute/fortblox/main/battleroyal.lua")
+    loadstring(br)()
+end
+if game.PlaceId == br2 then
+    local br = game:HttpGet("https://raw.githubusercontent.com/ThatsMyMute/fortblox/main/battleroyal.lua")
+    loadstring(br)()
+end
 
